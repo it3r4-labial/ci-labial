ad99c5d46d7eb0b37b2a7d856574874efa14110d
it3r4-labial <ladymlabial28@gmail.com>
2019-03-21 00:17:43 +0800

    Init Repo

---
+++ application/views/main.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Labial - Lab Activity</title>
+	<link rel="stylesheet" type="text/css" href="<?php echo base_url('assets/css/style.css'); ?>">
+</head>
+<body>
+	<h2>Testing</h2>
+	<script type="text/javascript" src="<?php echo base_url('assets/js/script.js'); ?>"></script>
+</body>
+</html>
